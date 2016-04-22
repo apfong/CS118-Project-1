1f160857efe063e7eaf63831b90a0575d190e99c
Alexander Fong <alpfong@gmail.com>
2016-04-22 05:15:50 +0000

    Implemented reading request and file transfer

--- web-server.cpp
+++ web-server.cpp
@@ -1,3 +1,4 @@
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -8,18 +9,20 @@
 #include <errno.h>
 #include <unistd.h>
 
+#include <fstream>
+
 #include <iostream>
 #include <sstream>
 using namespace std;
 
-#define MAXBYTES 1000 // most bytes we can receive at a time
+#define MAXBYTES 512 // most bytes we can receive at a time
 
 int main(int argc, char* argv[])
 {
 	//AF_INET is current default IP
 	string hostname = "localhost";
 	int port = 4000;
-	string filedir = ".";
+	string filedir = "./";
 	if(argc >= 2)
 		hostname = argv[1];
 	if(argc >= 3)
@@ -32,12 +35,6 @@ int main(int argc, char* argv[])
 	}
   // create a socket using TCP IP
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  struct addrinfo hints;
-  struct addrinfo *servinfo;
-  struct addrinfo *p;
-  //struct sockaddr_storage their_addr;
-  //socklen_t sin_size;
-
 
   // allow others to reuse the address
   int yes = 1;
@@ -51,6 +48,7 @@ int main(int argc, char* argv[])
   addr.sin_family = AF_INET;
   addr.sin_port = htons(port);     // short, network byte order
   addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+//  addr.sin_addr.s_addr = inet_addr(hostname);
   memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
 
   if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
@@ -66,8 +64,7 @@ int main(int argc, char* argv[])
 
   // read/write data from/into the connection
   bool isEnd = false;
-  char buf[MAXBYTES] = {0};
-  std::stringstream ss;
+  int bufSize = 1;
 
   while (!isEnd) {
     // accept a new connection
@@ -86,54 +83,98 @@ int main(int argc, char* argv[])
       ntohs(clientAddr.sin_port) << std::endl;
 
     if (!fork()) {   // if in here, we're on child process
-      close(sockfd); // closes listener for child
-      while (true) {
-        memset(buf, '\0', sizeof(buf));
+      char buf[MAXBYTES] = {0};
+      std::stringstream ss;
+      close(sockfd); // closes listener for new connections
+      memset(buf, '\0', sizeof(buf));
+      int bytesRecv;
+      size_t foundHeaderEnd;
 
-        if (recv(clientSockfd, buf, 20, 0) == -1) {
-          perror("recv");
-          return 5;
-        }
+      while ((bytesRecv = read(clientSockfd, buf, bufSize)) > 0) {
+        // TODO: Implement HTTP Timeout, say if server received part of
+        // message, but hasnt received the rest of the message for a long time
+        ss << buf;
 
-        /* TODO: implement HTTP transfer (recieving) here*/
-        ss << buf << std::endl;
-        std::cout << buf << std::endl;
-
-
-        if (send(clientSockfd, buf, 20, 0) == -1) {
-          perror("send");
-          return 6;
-        }
-
-        if (ss.str() == "close\n") 
+        foundHeaderEnd = ss.str().find("rnrn");
+        //std::cout << buf << "    " << to_string(foundHeaderEnd) << std::endl;
+        if (foundHeaderEnd != string::npos)
           break;
 
-        ss.str("");
+        memset(buf, '\0', sizeof(buf));
       }
-      close(clientSockfd);
-      exit(0);
-    }
-    else { // parent still listens for close
-      if (recv(clientSockfd, buf, 20, 0) == -1) {
+
+      // End of while loop
+      if (bytesRecv == -1) {
         perror("recv");
         return 5;
       }
 
-      /* Listen for close server */
-      ss << buf << std::endl;
-      std::cout << buf << std::endl;
-      
-      if (send(clientSockfd, buf, 20, 0) == -1) {
-        perror("send");
-        return 6;
+      // TODO: Create HttpMessage, if it fails, return a 400 bad request message
+      // do error checking on header fields such as version, if bad version !1.0 | !1.1
+      // then return 505 HTTP version not supported response
+      // if method (GET/POST) is not GET, then return 501 Not implemented response
+
+      // Preparing to open file
+      std::stringstream filestream;
+      std::string line;
+
+      std::string resFilename = ss.str().substr(0,15);
+      //ifstream resFile (resFilename);
+      streampos size;
+      char* memblock;
+      ifstream resFile (resFilename, ios::in|ios::binary|ios::ate);
+
+
+      // Dealing with root file request
+      if (resFilename == "/") {
+        // check if index exists and is unlocked and accessible
+        ifstream infile("index.html");
+        if (infile.good())
+          resFilename = "index.html";
       }
 
-      if (ss.str() == "close\n")
-        return 0;
+      // Prepending starting directory to requested filename
+      resFilename.insert(0, filedir);
 
-      ss.str("");
+      // Opening file
+      if (resFile.is_open()) {
+        /*
+        while (getline(resFile, line)) {
+          filestream << line << endl;
+        }
+        */
+        size = resFile.tellg();
+        memblock = new char [size];
+        resFile.seekg (0, ios::beg);
+        resFile.read (memblock, size);
+        resFile.close();
+
+        // Sending file back to client if it exists and was read correctly
+        // send back 200 OK status code
+        //if (send(clientSockfd, filestream.str().c_str(), filestream.str().size(), 0) == -1) {
+        if (send(clientSockfd, memblock, size, 0) == -1) {
+          perror("send");
+          return 6;
+        }
+        else
+          //std::cout << "sent: " << filestream.str().c_str() << endl;
+          std::cout << "sent: " << memblock << endl;
+
+        delete[] memblock;
+      }
+      // can't open file, return 404 here or other error
+      else {
+        size = 0;
+        std::cout << "404 ERROR\n";
+      }
+
+      printf("closing connection to %d\n", clientSockfd);
+      close(clientSockfd);
+      exit(0);
     }
-    close(clientSockfd);
+    else { // parent still listens for close
+      close(clientSockfd);
+    }
   }
   return 0;
 }
